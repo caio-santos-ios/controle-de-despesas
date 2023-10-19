9650a057be63ac4b35b2ce9c91eb99cc60fcfa44
caiosantos.dev@outlook.com <seu@email.com>
2023-10-19 15:07:38 -0300

    finalizando background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import './App.css'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { AiFillDelete } from 'react-icons/ai';
+import { toast } from 'react-toastify';
 
 interface INote {
   id?: number;
@@ -13,7 +14,6 @@ interface INote {
 
 function App() {
   const { register, handleSubmit} = useForm()
-  // const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('@notes')) ? JSON.parse(localStorage.getItem('@notes')) : [])
   const storedNotes = localStorage.getItem('@notes');
   const parsedNotes = storedNotes ? JSON.parse(storedNotes) : [];
   const [notes, setNotes] = useState(parsedNotes);
@@ -24,57 +24,89 @@ function App() {
       ...data
     }
     if(newData.valor && newData.tipo_nota){
+      toast.success('Nota Criada!', {
+        position: "top-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
       setNotes([...notes, newData])
       localStorage.setItem("@notes", JSON.stringify([...notes, newData]))
+      return
     }
+    toast.error('Deve preencher os campos!', {
+      position: "top-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      });
   }
   
   const remove = (id: string) => {
     const itemRemoved: INote[] = notes.filter((note: INote) => note.id != Number(id))
     setNotes(itemRemoved)
     localStorage.setItem("@notes", JSON.stringify(itemRemoved))
+    toast.success('Deletado', {
+      position: "top-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      });
   }
 
   return (
     <>
-      <main>
-        <Form className='d-flex aling-items-center justify-content-center p-3 mb-2 bg-secondary text-white' onSubmit={handleSubmit(onSubmit)}>
-          <Form.Group className="mb-3">
-                <Form.Label>Tipo de nota</Form.Label>
-                <InputGroup className="mb-3">
-                  <InputGroup.Text>R$</InputGroup.Text>
-                  <Form.Control {...register("valor")} aria-label="Amount (to the nearest dollar)" />
-                  <InputGroup.Text>.00</InputGroup.Text>
-                </InputGroup>  
-                <Form.Label>Tipo de nota</Form.Label>
-                <Form.Select {...register("tipo_nota")} aria-label="Default select example">
-                  <option value="">Escolha o tipo de nota</option>
-                  <option value="Entrada">Entrada</option>
-                  <option value="Saida">Saída</option>
-                </Form.Select>
-                <Button className='m-4' variant="primary" type="submit">Criar</Button>
-          </Form.Group>
-        </Form>
-        <div>
-          <ul className='bg-secondary text-white'>
+      <main className='container'>
+        <div className='d-flex flex-column flex-md-row justify-content-evenly align-items-center gap-3 py-5'>
+            <Form className='rounded p-3 bg-secondary text-white' onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group className="mb-3">
+                  <Form.Label>Tipo de nota</Form.Label>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>R$</InputGroup.Text>
+                    <Form.Control type='number' {...register("valor")} aria-label="Amount (to the nearest dollar)" />
+                    <InputGroup.Text>.00</InputGroup.Text>
+                  </InputGroup>  
+                  <Form.Label>Tipo de nota</Form.Label>
+                  <Form.Select {...register("tipo_nota")} aria-label="Default select example">
+                    <option value="">Escolha o tipo de nota</option>
+                    <option value="Entrada">Entrada</option>
+                    <option value="Saida">Saída</option>
+                  </Form.Select>
+                  <Button className='m-4' variant="primary" type="submit">Criar</Button>
+              </Form.Group>
+            </Form>
+
+            <ul style={{height: '30rem'}} className='rounded bg-secondary text-white d-flex flex-column align-items-center col-12 col-sm-9 col-md-7 col-lg-6 p-2 gap-3 overflow-y-auto'>
               {
                 notes.map((note: INote) => {
-                  return(
-                    <li key={note.id}>
-                      <span>R$ {note.valor}</span>
+                  return (
+                    <li key={note.id} className='rounded bg-warning text-dark d-flex align-items-center justify-content-between p-3 col-12 col-sm-12 col-md-9'>
+                      <span>R${note.valor}</span>
                       {
-                        note.tipo_nota == 'Entrada' ?
-                        <span className='tipo_nota' style={{background: 'green'}}>{note.tipo_nota}</span>
+                        note.tipo_nota == 'Entrada' ? 
+                        <span className='rounded p-2 col-4 text-center' style={{background: 'green'}}>{note.tipo_nota}</span>
                         :
-                        <span className='tipo_nota' style={{background: 'red'}}>{note.tipo_nota}</span>
+                        <span className='rounded p-2 col-4 text-center' style={{background: 'red'}}>{note.tipo_nota}</span>                        
                       }
-                      <button id={note.id?.toString()} onClick={(e) => remove(e.currentTarget.id)} className='btn_delete'>D</button>
+                      <AiFillDelete size={25} id={note.id?.toString()} onClick={(e: any) => remove(e.currentTarget.id)} />
                     </li>
                   )
-                })
+                }) 
               }
-          </ul>
-        </div>
+            </ul>
+        </div>          
       </main>
     </>
   )
