75a073b8fa16f7180ae3800bba81a56b4cfe9d2a
caiosantos.dev@outlook.com <seu@email.com>
2023-10-22 01:07:23 -0300

    fazendo ajustes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 
